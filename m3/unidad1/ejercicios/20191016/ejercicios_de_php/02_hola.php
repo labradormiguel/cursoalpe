43671bef54bfb6bb8e49efc1beca28c6a56b229e
miguel <miguel.labrador.sanchez+alpe@protonmail.com>
2019-10-16 14:10:53 +0200

    añadidos tres ejercicios de php

---
+++ m3/unidad1/ejercicios/20191016/ejercicios_de_php/02_hola.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Hola Mundo</title>
+    </head>
+    <body>
+        <h1>El famoso script Hello World</h1>
+        <p>
+        <?php
+            echo "Hola Mundo!";
+        ?>
+        </p>
+    </body>
+</html>
