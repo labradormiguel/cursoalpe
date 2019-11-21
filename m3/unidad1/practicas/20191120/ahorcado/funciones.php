c1e934702c90db04125d3b2753b1801705b16a37
miguel <miguel.labrador.sanchez+alpe@protonmail.com>
2019-11-21 13:55:58 +0100

    añadido 21 de noviembre

--- m3/unidad1/practicas/20191120/ahorcado/funciones.php
+++ m3/unidad1/practicas/20191120/ahorcado/funciones.php
@@ -1,34 +1,24 @@
 <?php
     function existe_caracter($cadena, $caracteres) {
         $salida = [];
-        $index = 0;
         
         for ($i=0; $i<strlen($cadena); $i++) {
             for ($j=0; $j<strlen($caracteres); $j++) {
                 if ($cadena[$i] == $caracteres[$j]) {
-                    $salida[$index]["posicion"] = $i;
-                    $salida[$index]["letra"] = $caracteres[$j];
-                    $index++;
+                    $salida[] = $i;
                     break;
                 }
             }
         }
         
-        if($index==0){
-         
-        return false;
-            
-        }else{
-        
         return $salida;
-        }
     }
 
     function crea_acertados($cadena, $array_acertados) {
         $salida = str_repeat("_", strlen($cadena));
-        for ($i=0; $i<strlen($cadena); $i++) {
+        for ($i=0; $i < strlen($cadena); $i++) {
             foreach ($array_acertados as $value) {
-                $salida[$value["posicion"]] = $value["letra"];
+                $salida[$value] = $cadena[$value];
             }
         }
         
@@ -48,3 +38,21 @@
         return $salida;
     }
     
+    function generateRandomString($length = 10) { 
+        return substr(str_shuffle("abcdefghijklmnopqrstuvwxyz "), 0, $length);
+    } 
+    /*
+    
+    $array_encontrados = existe_caracter("esto es una prueba", "e");
+    
+    var_dump($array_encontrados);
+    
+    echo crea_acertados("esto es una prueba", $array_encontrados);
+    
+    $array_encontrados = existe_caracter("esto es una prueba", "ea");
+    
+    var_dump($array_encontrados);
+    
+    echo separa_caracteres(crea_acertados("esto es una prueba", $array_encontrados));
+    
+    */
--- m3/unidad1/practicas/20191120/ahorcado/index.php
+++ m3/unidad1/practicas/20191120/ahorcado/index.php
@@ -1,75 +1,113 @@
 <?php
-    if(!isset($_SESSION["ahorcado"])){
     session_start();
-    $palabra= "ramon dejanos salir";
-    }
     require_once 'funciones.php';
+    
+    if (!isset($_SESSION["ahorcado"])) {
+        $_SESSION["ahorcado"] = generateRandomString(rand(3, 5));
+    }
+    
+    $imagenesAhorcado=[
+        "cabeza",
+        "cuerpo",
+        "brazoizq",
+        "brazoder",
+        "piernaizq",
+        "piernader",
+        "cabezamuerta"
+    ];
+    $salidaAhorcado="";
 ?>
 
 <html>
     <head>
         <title>Ahorcado</title>
-        <style>
-            div {
-                display: inline;
-            }
-            .fallos{
-                color: red;
-            }
-        </style>
+        <link rel="stylesheet" href="css/main.css"/>
     </head>
     <body>
         <?php
+            $_SESSION["mostrarFormulario"] = true;
         
-        if(isset($_GET["envio"])){
-            
-            $intento=$_GET["letra"];
+            if (isset($_GET["envio"])) {
 
-        
-        if(!isset($_SESSION["intentos"])){
-            $_SESSION["intentos"]="";
-        }
-        if(!isset($_SESSION["aciertos"])){
-            $_SESSION["aciertos"]="";
-        }
-        if(!isset($_SESSION["fallos"])){
-            $_SESSION["fallos"]="";
-        }
-        $_SESSION["intentos"].= $intento;
-        $encontrados = existe_caracter($palabra,$_SESSION["intentos"]);
-        $_SESSION["aciertos"] = separa_caracteres(crea_acertados($palabra,$encontrados));
-        if (!existe_caracter($palabra,$intento)){
-            $_SESSION["fallos"].=$intento;
-        }
+                $intento = $_GET["letra"];
 
-        echo $_SESSION["aciertos"];
-
-        }
-        
-        ?>
-
-        
-        
-        <p>
-            <form>
-                <label for="letra">Introduce una letra</label>
-                <input type="text" name="letra">
-                <button name="envio">Enviar</button>
-            </form>
-        </p>
-              
-
+                if (!isset($_SESSION["intentos"])) {
+                    $_SESSION["intentos"] = "";
+                }
                 
-        
-        <?php
-        
-        if(isset($_SESSION["fallos"])){
-        //$salida=0;
-        //for ($i=0;$i<strlen($_SESSION["fallos"]);$i++){
-            echo '<div class="fallos">'.$_SESSION["fallos"].' '.'</div>';
-            //echo '<div class="fallos">'.$salida.' '.'</div>';
-        }
+                if (!isset($_SESSION["fallos"])) {
+                    $_SESSION["fallos"] = "";
+                }
+                
+                $_SESSION["intentos"] .= $intento;
+                
+                $palabra = $_SESSION["ahorcado"];
+                
+                $encontrados = existe_caracter($palabra, $_SESSION["intentos"]);
+                
+                $aciertos = separa_caracteres(crea_acertados($palabra, $encontrados));
+                
+                if (existe_caracter($palabra, $intento) == []){
+                    $_SESSION["fallos"] .= $intento;
+                }
+
+                echo '<pre>' . $aciertos . '</pre>';
+                
+                if (strlen($palabra) == count($encontrados)) {
+                    echo "¡ACERTASTE!";
+                    unset($_SESSION["ahorcado"]);
+                    unset($_SESSION["intentos"]);
+                    unset($_SESSION["fallos"]);
+                    $_SESSION["mostrarFormulario"] = false;
+                }
+            for($c=0;$c<strlen($_SESSION["fallos"]);$c++){
+                        $salidaAhorcado.='<div id="' . $imagenesAhorcado[$c] . '"> </div>';
+            }
+            if (strlen($_SESSION["fallos"]) == 6)  {
+                $salidaAhorcado.='<div id="' . $imagenesAhorcado[6] . '"> </div>';
+                echo "¡PERDISTE!";
+                unset($_SESSION["ahorcado"]);
+                unset($_SESSION["intentos"]);
+                unset($_SESSION["fallos"]);
+                $_SESSION["mostrarFormulario"] = false;
+            }
+            } else {
+                echo '<pre>' . separa_caracteres(crea_acertados($_SESSION["ahorcado"], [])) . '</pre>';
+            }
+            
+
+       
         ?>
-        
+        <?php
+            if ($_SESSION["mostrarFormulario"]) {
+        ?>
+                <p>
+                    <form>
+                        <label for="letra">Introduce una letra</label>
+                        <input type="text" name="letra" maxlength="1" size="1">
+                        <button name="envio">Enviar</button>
+                    </form>
+                </p>
+        <?php
+            } else {
+        ?>
+                <p>
+                    <form>
+                        <button name="reiniciar">Reiniciar ahorcado</button>
+                    </form>
+                </p>
+        <?php
+            }
+        ?>
+        <?php
+            if (isset($_SESSION["fallos"])) {
+                echo '<div class="fallos">'.separa_caracteres($_SESSION["fallos"]).'</div>';
+            }
+        ?>
+                <div class="imagen">
+                    <div id="fondo">
+                        <?= $salidaAhorcado ?>
+                    </div> 
+                </div>
     </body>
 </html>
