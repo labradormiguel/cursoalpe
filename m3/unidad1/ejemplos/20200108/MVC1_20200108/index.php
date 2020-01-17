e1135a30a6216d804f0cc0b540e9693f464514ef
miguel <miguel.labrador.sanchez+alpe@protonmail.com>
2020-01-17 10:02:17 +0100

    corregido autoload del mal

--- m3/unidad1/ejemplos/20200108/MVC1_20200108/index.php
+++ m3/unidad1/ejemplos/20200108/MVC1_20200108/index.php
@@ -2,22 +2,19 @@
 
 session_start();
 
+/*
+
 spl_autoload_register(
   function ($pClassName) {
     spl_autoload(str_replace("\\", DIRECTORY_SEPARATOR, $pClassName));
   }
-);
+); */
 
 
-/*spl_autoload_register(function ($nombre_clase) {
-    str_replace("\", , $subject)
-    include $nombre_clase . '.php';
-});
-?>*/
+spl_autoload_register(
+	function ($nombre_clase) {
+		include str_replace("\\", DIRECTORY_SEPARATOR, $nombre_clase) . '.php';
+	});
 
-?>
 
-<?php
 new clases\Aplicacion();
-?>
-
